f5f377a7d9f8824ca2d3639e6f0e829471ee0119
MadanBlackbox85 <mkbh8585@gmail.com>
2025-05-16 09:52:30 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,16 @@
+public class Swap{
+
+public static void main(String[] args){
+
+String x = "water";
+String y = "acid";
+String temp;
+temp=x;
+x=y;
+y=temp;
+
+
+System.out.println("x:  "+x);
+System.out.println("y:  "+y);
+}
+}
